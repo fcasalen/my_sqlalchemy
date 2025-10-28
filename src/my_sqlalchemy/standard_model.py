78e9461bf42cc5c6061c0e16cd8cf1055d8f3c39
fcasalen <fcasalen@gmail.com>
2025-10-28 11:01:54 -0300

    chg: utils methods are being used only by standard model, so moved them to it

--- src/my_sqlalchemy/standard_model.py
+++ src/my_sqlalchemy/standard_model.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, DateTime, Integer
 from typing import Any
+from datetime import datetime
 
 from . import utils
 from .base import Base
@@ -18,16 +19,35 @@ class StandardModel(Base):
 
     def __repr__(self) -> str:
         """String representation of the model instance."""
-        data = utils.get_values_from_model_instance(self)
+        data = {}
+        for column in self.__table__.columns:
+            value = getattr(self, column.name)
+            if isinstance(value, datetime):
+                value = value.isoformat()
+            data[column.name] = value
         data = [f"{k}={v}" for k, v in data.items()]
         return f"<{', '.join(data)}>"
 
     @classmethod
     def get_columns(cls) -> dict[str, dict[str, Any]]:
-        """Get the columns of the table as a dictionary excluding the primary key."""
-        return utils.get_columns_detail_from_model(cls)
+        """Get columns as key and their detail (type, nullable, etc) as value in a dictionary.
 
-    @classmethod
-    def get_columns_type(cls) -> dict[str, str]:
-        """Get the columns types of the table as a dictionary."""
-        return utils.get_columns_type_from_model(cls)
+        Args:
+            model_class (DeclarativeMeta): The model class to extract columns from.
+
+        Returns:
+            dict[str, dict[str, Any]]: A dictionary with column names as keys and their details as values.
+        """
+        columns = {
+            column.name: {
+                "type": str(column.type),
+                "length": getattr(column.type, "length", None),
+                "nullable": column.nullable,
+                "default": column.default.arg if column.default is not None else None,
+            }
+            for column in cls.__table__.columns
+        }
+        for k in columns:
+            if callable(columns[k]["default"]):
+                columns[k]["default"] = f"function {columns[k]['default'].__name__}"
+        return columns
--- src/my_sqlalchemy/utils.py
+++ src/my_sqlalchemy/utils.py
@@ -1,69 +1,6 @@
 from datetime import UTC, datetime
-from typing import Any
-from sqlalchemy.orm.decl_api import DeclarativeMeta
 
 
 def utc_now():
     """Return current UTC timestamp. Callable for SQLAlchemy default values."""
     return datetime.now(UTC)
-
-
-def get_values_from_model_instance(model_instance: DeclarativeMeta) -> dict[str, Any]:
-    """Get columns as keys and their values from a model instance as values in a dictionary.
-    Datetime values are converted to ISO format strings.
-
-    Args:
-        model_instance (DeclarativeMeta): The model instance to extract values from.
-
-    Returns:
-        dict[str, Any]: A dictionary with column names as keys and their values as values.
-    """
-    data = {}
-    for column in model_instance.__table__.columns:
-        value = getattr(model_instance, column.name)
-        if isinstance(value, datetime):
-            value = value.isoformat()
-        data[column.name] = value
-    return data
-
-
-def get_columns_detail_from_model(
-    model_class: DeclarativeMeta,
-) -> dict[str, dict[str, Any]]:
-    """Get columns as key and their detail (type, nullable, etc) as value in a dictionary, excluding primary key.
-
-    Args:
-        model_class (DeclarativeMeta): The model class to extract columns from.
-
-    Returns:
-        dict[str, dict[str, Any]]: A dictionary with column names as keys and their details as values.
-    """
-    columns = {
-        column.name: {
-            "type": str(column.type),
-            "length": getattr(column.type, "length", None),
-            "nullable": column.nullable,
-            "default": column.default.arg if column.default is not None else None,
-        }
-        for column in model_class.__table__.columns
-        if not column.primary_key
-    }
-    for k in columns:
-        if callable(columns[k]["default"]):
-            columns[k]["default"] = f"function {columns[k]['default'].__name__}"
-    return columns
-
-
-def get_columns_type_from_model(model_class: DeclarativeMeta) -> dict[str, str]:
-    """Get columns names as keys and their types as values in a dictionary.
-
-    Args:
-        model_class (DeclarativeMeta): The model class to extract columns from.
-
-    Returns:
-        dict[str, str]: A dictionary with column names as keys and their types as values.
-    """
-    columns = {
-        column.name: str(column.type) for column in model_class.__table__.columns
-    }
-    return columns
